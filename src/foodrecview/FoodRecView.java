2eabce108ef7c81722defc83ab799fdeac95e5fc
siqitang <siqitang@104.39.55.92>
2017-10-01 13:40:43 -0400

    Created ViewNotifications, CRUDUserProfile, GetFoodRec

--- src/foodrecview/FoodRecView.java
+++ src/foodrecview/FoodRecView.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package notificationview;
+package foodrecview;
 
 /**
  *
  * @author siqitang
  */
-public class NotificationView {
+public class FoodRecView {
     
 }
